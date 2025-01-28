0a5bcf2e6f903aa42e477244bcb3a268ce84eac0
AnishLohiya <anishlohiya196@gmail.com>
2025-01-28 20:32:50 +0530

    misc changes

--- src/app/warf-calculator/warf-calculator.component.ts
+++ src/app/warf-calculator/warf-calculator.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CLOFund } from '../fund.model';
 import { WarfCalculatorService } from '../warf-calculator.service';
 
 @Component({
   selector: 'app-warf-calculator',
-  imports: [NgFor, NgIf, FormsModule],
+  imports: [NgFor, NgIf, FormsModule, NgClass],
   templateUrl: './warf-calculator.component.html',
   styleUrl: './warf-calculator.component.scss',
 })
 export class WarfCalculatorComponent {
-  funds: CLOFund[] = [];
+  funds: CLOFund[] = [];  
+  fundColumns = ['WAS', 'Diversity', 'WARF', 'MRR'];
+  selectedFund!: CLOFund | null;
+  selectedFundData!: CLOFund["Results"];
 
   constructor(private warfService: WarfCalculatorService) {}
 
@@ -19,35 +22,6 @@ export class WarfCalculatorComponent {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.loadData();
-  }
-
-  fundColumns = ['WAS', 'Diversity', 'WARF', 'MRR'];
-  selectedFund!: CLOFund | null;
-  selectedFundData!: {
-   Result: {
-      WAS: number;
-      Diversity: number;
-      WARF: number;
-      MRR: number;
-    };
-    Limit: {
-      WAS: number;
-      Diversity: number;
-      WARF: number;
-      MRR: number;
-    };
-    Cushion: {
-      WAS: number;
-      Diversity: number;
-      WARF: number;
-      MRR: number;
-    };
-    result: {
-      WAS: string;
-      Diversity: string;
-      WARF: string;
-      MRR: string;
-    }
   }
 
   loadData() {
@@ -77,25 +51,24 @@ export class WarfCalculatorComponent {
     },
   };
 
- onFundSelect(event: Event): void {
-  const selectedFundName = (event.target as HTMLSelectElement).value;
-  this.selectedFund =
-    this.funds.find((fund) => fund.fund === selectedFundName) || null;
+  onFundSelect(event: Event): void {
+    const selectedFundName = (event.target as HTMLSelectElement).value;
+    this.selectedFund =
+      this.funds.find((fund) => fund.fund === selectedFundName) || null;
 
-  if (this.selectedFund?.Results?.Result) {
-    this.selectedFundData = this.selectedFund.Results;
+    if (this.selectedFund?.Results?.Result) {
+      this.selectedFundData = this.selectedFund.Results;
+    }
+
+    console.log(this.selectedFundData);
+
+    if (this.selectedFund) {
+      this.adjustments.Result.WAS = 0;
+      this.adjustments.Result.Diversity = 0;
+      this.adjustments.Result.MRR = 0;
+      this.updateAdjustedValues();
+    }
   }
-
-  console.log(this.selectedFundData);
-
-  if (this.selectedFund) {
-    this.adjustments.Result.WAS = 0;
-    this.adjustments.Result.Diversity = 0;
-    this.adjustments.Result.MRR = 0;
-    this.updateAdjustedValues();
-  }
-}
-
 
   updateAdjustedValues(): void {
     if (this.selectedFund) {
@@ -103,5 +76,20 @@ export class WarfCalculatorComponent {
       this.adjustedValues.Result.Diversity = this.selectedFundData.Result.Diversity + this.adjustments.Result.Diversity;
       this.adjustedValues.Result.MRR = this.selectedFundData.Result.MRR + this.adjustments.Result.MRR;
     }
+  }
+
+  resetSliders(): void {
+    this.adjustments.Result.WAS = 0;
+    this.adjustments.Result.Diversity = 0;
+    this.adjustments.Result.MRR = 0;
+    this.updateAdjustedValues();
+  }
+
+  resultColorClass(value: string): string {
+    return value === 'PASS' ? 'text-green-500' : 'text-red-500';
+  }
+
+  sliderColorClass(value: number): string {
+    return value == 0 ? 'text-gray-400' : value > 0 ? 'text-green-500' : 'text-red-500';
   }
 }
